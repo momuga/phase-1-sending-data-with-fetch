23f880e91f669cad24bab7876406d6f8eb5e4475
Mark <mark.omuga@student.moringaschool.com>
2023-10-26 15:24:39 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Add your code here
+
+	const requestContent =	{
+		name: "Mark",
+		email: "momuga@mail.com",
+	};
+
+ 	function sumbitData() {
+ 	
+		const requestData = {
+		method: "POST",
+	 	headers: {
+			"Content-Type": "application/json",
+			Accept: "application/json",
+		},
+			body: JSON.stringify(requestContent),
+		};
+
+		return fetch("http://localhost:3000/users", requestData)
+		.then(function (response)
+		{
+			return response.json();
+		})
+		.then(function (object)
+		{
+			console.log(object);
+		})
+		.catch(function (error) {
+			alert("Error");
+			console.log(error.message);
+		});
+	}
